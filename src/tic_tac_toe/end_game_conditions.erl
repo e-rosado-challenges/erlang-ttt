596e0f707ffa0778d1c8117bf623d1ab2db01f4d
ericrosado <erosado@8thlight.com>
2018-09-16 00:06:00 -0500

    Add forward_diagonal_win condition.

--- src/tic_tac_toe/end_game_conditions.erl
+++ src/tic_tac_toe/end_game_conditions.erl
@@ -1,5 +1,6 @@
 -module(end_game_conditions).
--export([is_tie_game/1, is_horizontal_win/1, is_vertical_win/1]).
+-export([is_tie_game/1, is_horizontal_win/1,
+         is_vertical_win/1, is_forward_diagonal_win/1]).
 
 is_tie_game([]) -> true;
 is_tie_game([Space|_Spaces]) when is_integer(Space) -> false;
@@ -11,10 +12,12 @@ is_horizontal_win(Board) ->
   RowStatus = lists:map(fun(Row) -> is_the_same_marker(Row) end, Rows),
   lists:member(true, RowStatus).
 
-get_rows(Board, Offset, BoardLength, Rows) when Offset < length(Board)  ->
-  Row = lists:sublist(Board, Offset, BoardLength),
-  get_rows(Board, Offset + BoardLength , BoardLength, [Row| Rows]);
-get_rows(_Board, _Offset, _BoardLength, Rows) -> Rows.
+get_rows(Board, StartingPoint, TotalLength,
+         Rows) when StartingPoint < length(Board)  ->
+  Row = lists:sublist(Board, StartingPoint, TotalLength),
+  NewStartingPoint = StartingPoint + TotalLength,
+  get_rows(Board, NewStartingPoint, TotalLength, [Row| Rows]);
+get_rows(_Board, _StartingPoint, _TotalLength, Rows) -> Rows.
 
 is_vertical_win(Board) ->
   BoardLength = get_board_length(Board),
@@ -24,12 +27,25 @@ is_vertical_win(Board) ->
                            end, Columns),
   lists:member(true, ColumnStatus).
 
-get_columns(Board, Offset, BoardSize,
-            Incrementer, Columns) when Offset =< Incrementer ->
-  Spaces = lists:seq(Offset, BoardSize, Incrementer),
+get_columns(Board, StartingPoint, EndPoint,
+            Incrementer, Columns) when StartingPoint =< Incrementer ->
+  Spaces = lists:seq(StartingPoint, EndPoint, Incrementer),
   Column = lists:map(fun(Space) -> lists:nth(Space, Board) end, Spaces),
-  get_columns(Board, Offset + 1, BoardSize, Incrementer, [Column|Columns]);
-get_columns(_Board, _Offset, _BoardSize, _Incrementer, Sets) -> Sets.
+  NewStartingPoint = StartingPoint + 1,
+  get_columns(Board, NewStartingPoint, EndPoint, Incrementer, [Column|Columns]);
+get_columns(_Board, _StartingPoint, _EndPoint, _Incrementer, Sets) -> Sets.
+
+is_forward_diagonal_win(Board) ->
+  BoardLength = get_board_length(Board),
+  Diagonal = get_forward_diagonal(Board, BoardLength),
+  is_the_same_marker(Diagonal).
+
+get_forward_diagonal(Board, BoardLength) ->
+  Incrementer = BoardLength - 1,
+  StartingPoint = BoardLength,
+  EndPoint = length(Board) - Incrementer,
+  Spaces = lists:seq(StartingPoint, EndPoint, Incrementer),
+  lists:map(fun(Space) -> lists:nth(Space, Board) end, Spaces).
 
 get_board_length(Board) ->
   Spaces = length(Board),
--- test/end_game_conditions_tests.erl
+++ test/end_game_conditions_tests.erl
@@ -1,7 +1,7 @@
 -module(end_game_conditions_tests).
 -include_lib("eunit/include/eunit.hrl").
 -compile({nowarn_unused_function, [is_tie_game_test/0, is_horizontal_win_test/0, 
-                                   is_vertical_win_test/0]}).
+                                   is_vertical_win_test/0, is_forward_diagonal_win_test/0]}).
 
 is_tie_game_test() ->
   [{"it will return true if there is no winner and all spaces on the board are occupied",
@@ -43,4 +43,18 @@ is_vertical_win_test() ->
                  end_game_conditions:is_horizontal_win([x,2,x,4,5,6,7,o,o])),
     ?assertEqual(false,
                  end_game_conditions:is_horizontal_win([1,o,o,4,x,6,7,x,x]))
+  }].
+
+is_forward_diagonal_win_test() ->
+  [{"it will return true if there is a vertical win",
+    ?assertEqual(true,
+                 end_game_conditions:is_forward_diagonal_win([1,2,x,4,x,6,x,o,o])),
+    ?assertEqual(true,
+                 end_game_conditions:is_forward_diagonal_win([x,2,o,4,o,6,o,x,x]))
+  },
+  {"it will return false if there is not a vertical win",
+    ?assertEqual(false,
+                 end_game_conditions:is_forward_diagonal_win([x,2,x,4,5,6,7,o,o])),
+    ?assertEqual(false,
+                 end_game_conditions:is_forward_diagonal_win([1,o,o,4,x,6,7,x,x]))
   }].
