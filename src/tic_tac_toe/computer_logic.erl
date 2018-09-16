455bedeb52f31cbd62a133b46b63ea5a5eaa42fc
ericrosado <erosado@8thlight.com>
2018-09-16 18:15:51 -0500

    Add ability to switch game markers.

--- src/tic_tac_toe/computer_logic.erl
+++ src/tic_tac_toe/computer_logic.erl
@@ -1,8 +1,13 @@
 -module(computer_logic).
--export([get_available_spaces/2]).
+-export([get_available_spaces/2, switch_marker/1]).
+-define(PLAYER, x).
+-define(COMPUTER, o).
 
 get_available_spaces([Space|Spaces], AvailableSpaces) when is_integer(Space) ->
   get_available_spaces(Spaces, [Space|AvailableSpaces]);
 get_available_spaces([_Space|Spaces], AvailableSpaces) ->
   get_available_spaces(Spaces, AvailableSpaces);
-get_available_spaces([], AvailableSpaces) -> lists:reverse(AvailableSpaces).
+get_available_spaces([], AvailableSpaces) -> AvailableSpaces.
+
+switch_marker(Marker) when Marker =:= ?PLAYER -> ?COMPUTER;
+switch_marker(_Marker) -> ?PLAYER.
--- test/computer_logic_tests.erl
+++ test/computer_logic_tests.erl
@@ -1,11 +1,19 @@
 -module(computer_logic_tests).
 -include_lib("eunit/include/eunit.hrl").
--compile({nowarn_unused_function, [get_available_spaces_test/0]}).
+-compile({nowarn_unused_function, [get_available_spaces_test/0, switch_marker_test/0]}).
 
 get_available_spaces_test() ->
   {"it will return available spaces when given a Board.",
-    ?assertEqual([2,3,4,5,6,7,8,9],
+    ?assertEqual([9,8,7,6,5,4,3,2],
                  computer_logic:get_available_spaces([x,2,3,4,5,6,7,8,9], [])),
-    ?assertEqual([2,3,5,6,8,9],
+    ?assertEqual([9,8,6,5,3,2],
                  computer_logic:get_available_spaces([x,2,3,o,5,6,x,8,9], []))
+  }.
+
+switch_marker_test() ->
+  {"it will switch the marker to the other available marker",
+    ?assertEqual(o,
+                 computer_logic:switch_marker(x)),
+    ?assertEqual(x,
+                 computer_logic:switch_marker(o))
   }.
