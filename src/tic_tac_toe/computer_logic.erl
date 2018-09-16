d63f5aad2dd9eba9f15b06683c3029f4fb65685d
ericrosado <erosado@8thlight.com>
2018-09-16 18:58:26 -0500

    Add ability to get an end game score.

--- src/tic_tac_toe/computer_logic.erl
+++ src/tic_tac_toe/computer_logic.erl
@@ -1,5 +1,6 @@
 -module(computer_logic).
--export([get_available_spaces/2, switch_marker/1, place_marker/3]).
+-export([get_available_spaces/2, switch_marker/1, place_marker/3,
+        get_score/2]).
 -define(PLAYER, x).
 -define(COMPUTER, o).
 
@@ -16,3 +17,15 @@ place_marker(Board, AvailableSpace, Marker) ->
   lists:sublist(Board, AvailableSpace - 1)
          ++ [Marker]
          ++ lists:nthtail(AvailableSpace, Board).
+
+get_score(Board, Marker) ->
+  MaximumScore = 10,
+  MinimumScore = -10,
+  TieScore = 0,
+  {Condition, Status} = end_game_conditions:is_game_over(Board),
+
+case {Condition, Status, Marker} of
+  {tie_game, true, _} -> TieScore;
+  {_, true, ?PLAYER} -> MinimumScore;
+  {_, true, ?COMPUTER} -> MaximumScore
+end.
--- test/computer_logic_tests.erl
+++ test/computer_logic_tests.erl
@@ -1,7 +1,7 @@
 -module(computer_logic_tests).
 -include_lib("eunit/include/eunit.hrl").
 -compile({nowarn_unused_function, [get_available_spaces_test/0, switch_marker_test/0,
-                                   place_marker_test/0]}).
+                                   place_marker_test/0, get_score_test/0]}).
 
 get_available_spaces_test() ->
   {"it will return available spaces when given a Board.",
@@ -29,4 +29,18 @@ place_marker_test() ->
                  computer_logic:place_marker(_Board = [x,2,x,4,5,o,7,o,9], 
                                              _AvailableSpace = 4, 
                                              _Marker = x))
+  }.
+
+get_score_test() ->
+  {"it will return 10 for computer win",
+    ?assertEqual(10,
+                 computer_logic:get_score([o,o,o,x,x,6,7,8,9], o))
+  },
+  {"it will return -10 for player win",
+    ?assertEqual(-10,
+                 computer_logic:get_score([x,x,o,x,o,6,x,8,9], x))
+  },
+  {"it will return 0 for a tie",
+    ?assertEqual(0,
+                 computer_logic:get_score([x,o,x,x,o,x,o,x,o], x))
   }.
