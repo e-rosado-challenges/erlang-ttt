b07ae1f8c839178b9bb02b305142a3f2f9dae082
ericrosado <erosado@8thlight.com>
2018-09-15 21:57:24 -0500

    Add horizontal_win condition.

--- src/tic_tac_toe/end_game_conditions.erl
+++ src/tic_tac_toe/end_game_conditions.erl
@@ -1,6 +1,26 @@
 -module(end_game_conditions).
--export([is_tie_game/1]).
+-export([is_tie_game/1, is_horizontal_win/1]).
 
 is_tie_game([]) -> true;
 is_tie_game([Space|_Spaces]) when is_integer(Space) -> false;
 is_tie_game([_Space|Spaces]) -> is_tie_game(Spaces).
+
+is_horizontal_win(Board) ->
+  BoardLength = get_board_length(Board),
+  Rows = get_rows(Board, 1, BoardLength, []),
+  RowStatus = lists:map(fun(Row) -> is_the_same_marker(Row) end, Rows),
+  lists:member(true, RowStatus).
+
+get_rows(Board, Offset, BoardLength, Rows) when Offset < length(Board)  ->
+  Row = lists:sublist(Board, Offset, BoardLength),
+  get_rows(Board, Offset + BoardLength , BoardLength, [Row| Rows]);
+get_rows(_Board, _Offset, _BoardLength, Rows) ->
+  Rows.
+
+get_board_length(Board) ->
+  Spaces = length(Board),
+  trunc(math:sqrt(Spaces)).
+
+is_the_same_marker([]) -> true;
+is_the_same_marker([Space1,Space2|_Spaces]) when Space1 /= Space2 -> false;
+is_the_same_marker([_Space|Spaces]) -> is_the_same_marker(Spaces).
--- test/end_game_conditions_tests.erl
+++ test/end_game_conditions_tests.erl
@@ -1,6 +1,6 @@
 -module(end_game_conditions_tests).
 -include_lib("eunit/include/eunit.hrl").
--compile({nowarn_unused_function, [is_tie_game_test/0]}).
+-compile({nowarn_unused_function, [is_tie_game_test/0, is_horizontal_win_test/0]}).
 
 is_tie_game_test() ->
   [{"it will return true if there is no winner and all spaces on the board are occupied",
@@ -14,4 +14,18 @@ is_tie_game_test() ->
                  end_game_conditions:is_tie_game([x,o,x,x,o,x,o,8,o])),
     ?assertEqual(false, 
                  end_game_conditions:is_tie_game([1,o,x,x,o,x,o,x,o]))
+  }].
+
+is_horizontal_win_test() ->
+  [{"it will return true if there is a horizontal win",
+    ?assertEqual(true,
+                 end_game_conditions:is_horizontal_win([x,x,x,4,5,6,7,o,o])),
+    ?assertEqual(true,
+                 end_game_conditions:is_horizontal_win([x,x,3,o,o,o,7,x,x]))
+  },
+  {"it will return false if there is not a horizontal win",
+    ?assertEqual(false,
+                 end_game_conditions:is_horizontal_win([x,2,x,4,5,6,7,o,o])),
+    ?assertEqual(false,
+                 end_game_conditions:is_horizontal_win([1,o,o,4,x,6,7,x,x]))
   }].
