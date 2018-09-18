49053f2bf40413cef6577d8ab4201810f1dabb39
ericrosado <erosado@8thlight.com>
2018-09-18 00:44:27 -0500

    Add ability to allow computer to make best move and return the new board.

--- src/tic_tac_toe/computer_logic.erl
+++ src/tic_tac_toe/computer_logic.erl
@@ -1,28 +1,40 @@
 -module(computer_logic).
 -export([get_available_spaces/2, switch_marker/1, place_marker/3,
-        get_score/2, get_board_states/2, get_board_state_score/2]).
+        get_score/3, get_board_states/2, get_board_state_score/2,
+        make_best_move/2]).
 -define(PLAYER, x).
 -define(COMPUTER, o).
+
+make_best_move(Board, Marker) ->
+  BoardStates = get_board_states(Board, Marker),
+  Scores = get_scores(BoardStates, Marker),
+  StatesAndScores = lists:zip(Scores, BoardStates),
+  SortedStatesAndValues = lists:keysort(1, StatesAndScores),
+  [{_Score, BestBoard}|_Boards] = lists:reverse(SortedStatesAndValues),
+  BestBoard.
 
 get_board_state_score(Board, Marker) ->
   IsGameOver = end_game_conditions:is_game_over(Board),
   get_board_state_score(Board, Marker, IsGameOver).
 
-get_board_state_score(_Board, Marker, {Condition, GameOver})
+get_board_state_score(Board, Marker, {Condition, GameOver})
 when GameOver =:= true ->
   WinningMarker = switch_marker(Marker),
-  get_score(Condition, WinningMarker);
+  get_score(Condition, WinningMarker, Board);
 get_board_state_score(Board, Marker, _IsGameOver) ->
   BoardStates = get_board_states(Board, Marker),
-  Scores = lists:map(fun(BoardState) ->
-                      NextMarker = switch_marker(Marker),
-                      get_board_state_score(BoardState, NextMarker)
-                     end, BoardStates),
+  Scores = get_scores(BoardStates, Marker),
   get_best_score(Scores, Marker).
 
+get_scores(BoardStates, Marker) ->
+  lists:map(fun(BoardState) ->
+                      NextMarker = switch_marker(Marker),
+                      get_board_state_score(BoardState, NextMarker)
+                     end, BoardStates).
+
 get_best_score(Scores, Marker) when Marker =:= ?PLAYER ->
-  lists:max(lists:flatten(Scores));
-get_best_score(Scores, _Marker) -> lists:min(lists:flatten(Scores)).
+  lists:min(lists:flatten(Scores));
+get_best_score(Scores, _Marker) -> lists:max(lists:flatten(Scores)).
 
 get_board_states(Board, Marker) -> get_board_states(Board, Board, [], Marker).
 
@@ -33,6 +45,18 @@ when is_integer(Space) ->
   get_board_states(Board, Spaces, [NewBoardState|BoardStates], Marker);
 get_board_states(Board, [_Space|Spaces], BoardStates, Marker) ->
   get_board_states(Board, Spaces, BoardStates, Marker).
+
+get_score(Condition, Marker, Board) ->
+  MaximumScore = 10,
+  MinimumScore = -10,
+  TieScore = 0,
+  Depth = length(Board) - length(get_available_spaces(Board, [])),
+
+  case {Condition, Marker} of
+    {tie_game, _} -> TieScore;
+    {_, ?PLAYER} -> MinimumScore + Depth;
+    {_, ?COMPUTER} -> MaximumScore - Depth
+  end.
 
 get_available_spaces([Space|Spaces], AvailableSpaces) when is_integer(Space) ->
   get_available_spaces(Spaces, [Space|AvailableSpaces]);
@@ -47,14 +71,3 @@ place_marker(Board, AvailableSpace, Marker) ->
   lists:sublist(Board, AvailableSpace - 1)
          ++ [Marker]
          ++ lists:nthtail(AvailableSpace, Board).
-
-get_score(Condition, Marker) ->
-  MaximumScore = 10,
-  MinimumScore = -10,
-  TieScore = 0,
-
-  case {Condition, Marker} of
-    {tie_game, _} -> TieScore;
-    {_, ?PLAYER} -> MinimumScore;
-    {_, ?COMPUTER} -> MaximumScore
-  end.
--- test/computer_logic_tests.erl
+++ test/computer_logic_tests.erl
@@ -1,8 +1,7 @@
 -module(computer_logic_tests).
 -include_lib("eunit/include/eunit.hrl").
 -compile({nowarn_unused_function, [get_available_spaces_test/0, switch_marker_test/0,
-                                   place_marker_test/0, get_score_test/0,
-                                   get_board_states_test/0, get_board_state_score_test/0]}).
+                                   place_marker_test/0, make_best_move_test/0]}).
 
 get_available_spaces_test() ->
   {"it will return available spaces when given a Board.",
@@ -32,40 +31,44 @@ place_marker_test() ->
                                              _Marker = x))
   }.
 
-get_score_test() ->
-  [{"it will return 10 for computer win",
-    ?assertEqual(10,
-                 computer_logic:get_score(_Condition = horizontal_win,
-                                          _Marker = o))
+make_best_move_test() ->
+  {"it will block a horizontal win",
+    ?assertEqual([x,x,o,4,5,6,7,o,9],
+                  computer_logic:make_best_move(_Board = [x,x,3,4,5,6,7,o,9],
+                                               _Marker = o))
   },
-  {"it will return -10 for player win",
-    ?assertEqual(-10,
-                 computer_logic:get_score(_Condition = vertical_win,
-                                          _Marker = x))
+  {"it will block a vertical win",
+    ?assertEqual([x,2,3,x,5,6,o,o,9],
+                  computer_logic:make_best_move(_Board = [x,2,3,x,5,6,7,o,9],
+                                               _Marker = o))
   },
-  {"it will return 0 for a tie",
-    ?assertEqual(0,
-                 computer_logic:get_score(_Condition = tie_game,
-                                          _Marker = o))
-  }].
-
-get_board_states_test() ->
-  {"it will return the remaining board states",
-    ?assertEqual([[x,o,x,x,o,x,o,x,o]],
-                 computer_logic:get_board_states([x,o,x,x,o,x,o,x,9], o)),
-    ?assertEqual([[x,o,3,x,o,x,o,x,x], [x,o,x,x,o,x,o,x,9]],
-                 computer_logic:get_board_states([x,o,3,x,o,x,o,x,9], x))
-  }.
-
-get_board_state_score_test() -> 
-  {"it will return the min or max score for a given board state and marker",
-    ?assertEqual(0,
-                 computer_logic:get_board_state_score(_TieGameBoard = [x,o,x,x,o,x,o,x,9], 
-                                                      _Marker = o)),
-    ?assertEqual(-10,
-                 computer_logic:get_board_state_score(_XWinsBoard = [x,2,x,o,o,x,o,x,o], 
-                                                      _Marker = x)),
-    ?assertEqual(10,
-                 computer_logic:get_board_state_score(_OWinsBoard = [o,x,x,x,o,x,x,o,9], 
-                                                      _Marker = o))
+  {"it will block a forward diagonal win",
+    ?assertEqual([1,2,o,4,x,6,x,o,9],
+                  computer_logic:make_best_move(_Board = [1,2,3,4,x,6,x,o,9],
+                                               _Marker = o))
+  },
+  {"it will block a backwards diagonal win",
+    ?assertEqual([o,2,3,4,x,6,7,o,x],
+                  computer_logic:make_best_move(_Board = [1,2,3,4,x,6,7,o,x],
+                                               _Marker = o))
+  },
+  {"it will take the horizontal win",
+    ?assertEqual([o,o,o,4,x,6,x,x,9],
+                  computer_logic:make_best_move(_Board = [o,2,o,4,x,6,x,x,9],
+                                               _Marker = o))
+  },
+  {"it will take the vertical win",
+    ?assertEqual([o,2,3,o,x,x,o,x,9],
+                  computer_logic:make_best_move(_Board = [o,2,3,o,x,x,7,x,9],
+                                               _Marker = o))
+  },
+  {"it will take the forward diagonal win",
+    ?assertEqual([x,x,o,x,o,6,o,x,9],
+                  computer_logic:make_best_move(_Board = [x,x,o,x,o,6,7,x,9],
+                                               _Marker = o))
+  },
+  {"it will take the backwards diagonal win",
+    ?assertEqual([o,x,3,x,o,6,x,8,o],
+                  computer_logic:make_best_move(_Board = [o,x,3,x,o,6,x,8,9],
+                                               _Marker = o))
   }.
