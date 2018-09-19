2e7e1d4c4c961633b45c6f7c8143286e50ae029e
ericrosado <erosado@8thlight.com>
2018-09-19 14:44:54 -0500

    Add move validation to determine if the space is already taken.

--- src/tic_tac_toe/io_handler.erl
+++ src/tic_tac_toe/io_handler.erl
@@ -1,7 +1,7 @@
 -module(io_handler).
 -export([print/1, print_greeting/0, invalid_entry/0,
-         print_winner/1, print_tie/0, read_input/1, request_space/0,
-         request_space/2]).
+         print_winner/1, print_tie/0, read_input/1, request_space/1,
+         request_space/3, validate_move/3]).
 
 print(Item) -> io:fwrite(Item).
 
@@ -15,14 +15,23 @@ print_tie() -> print(prompt:tie()).
 
 read_input(Prompt) -> io:fread(Prompt, "~s").
 
-request_space() ->
-  request_space(read_input(prompt:choose_space()), request_space()).
+request_space(AvailableSpaces) ->
+  request_space(AvailableSpaces, read_input(prompt:choose_space()),
+                request_space(AvailableSpaces)).
 
-request_space({_Status, [Input]}, RequestSpace) ->
+request_space(AvailableSpaces, {_Status, [Input]}, RequestSpace) ->
   try list_to_integer(Input) of
-    Result -> Result
+    Move ->
+      AvailableMove = lists:member(Move, AvailableSpaces),
+      validate_move(AvailableMove, RequestSpace, Move)
   catch
-    error:_ -> RequestSpace
+    error:_ ->
+      invalid_entry(),
+      RequestSpace
   end.
 
-
+validate_move(AvailableMove, _RequestSpace, Move)
+when AvailableMove -> Move;
+validate_move(_AvailableMove, RequestSpace, _Move) ->
+  invalid_entry(),
+  RequestSpace.
--- test/io_handler_test.erl
+++ test/io_handler_test.erl
@@ -3,12 +3,20 @@
 -compile({nowarn_unused_function, [request_space_test/0]}).
 
 request_space_test() ->
-  {"it will return the user input if it is an integer",
-    ?assertEqual(1, io_handler:request_space(_InputStatus = {ok,["1"]},
+  {"it will return the user move if it is an integer and is an available space.",
+    ?assertEqual(1, io_handler:request_space(_AvailableSpaces = [1,2,3],
+                                             _InputStatus = {ok,["1"]},
                                              _CatchFunction = io_handler:invalid_entry()))
   },
-  {"it will not return the user input if it is not an integer",
+  {"it will return invalid_entry if the user move is not available",
     ?assertEqual(io_handler:invalid_entry(), 
-                 io_handler:request_space(_InputStatus = {ok,["Hello"]},
+                 io_handler:request_space(_AvailableSpaces = [1,2,3],
+                                          _InputStatus = {ok,["4"]},
+                                          _CatchFunction = io_handler:invalid_entry()))
+  },
+  {"it will return invalid_entry if the user input is not an integer",
+    ?assertEqual(io_handler:invalid_entry(),
+                 io_handler:request_space(_AvailableSpaces = [1,2,3],
+                                          _InputStatus = {ok,["Hello"]},
                                           _CatchFunction = io_handler:invalid_entry()))
   }.
