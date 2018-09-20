f2662bee4a583abb07e0755adf78253be2711f6c
e-rosado <edavidrosado@gmail.com>
2018-09-20 14:34:09 -0500

    Remove start prompt from prompt module.

--- src/tic_tac_toe/prompt.erl
+++ src/tic_tac_toe/prompt.erl
@@ -1,12 +1,9 @@
 -module(prompt).
--export([greeting/0, start/0, choose_space/0, invalid_entry/0,
+-export([greeting/0, choose_space/0, invalid_entry/0,
          win/1, tie/0]).
 
 greeting() ->
   "Welcome to Tic Tac Toe!\n".
-
-start() ->
-  "Please type 'start' and then press enter to begin.\n".
 
 choose_space() ->
   "Please choose an empty space, designated by a number.\n".
