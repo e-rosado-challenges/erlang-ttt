456a164cc0fea7b1efa045e22eba82eb34aa1b2b
ericrosado <erosado@8thlight.com>
2018-09-19 20:21:11 -0500

    Add ability to get updated board from computer move.

--- src/tic_tac_toe/game.erl
+++ src/tic_tac_toe/game.erl
@@ -1,7 +1,10 @@
 -module(game).
--export([get_player_move/2]).
+-export([get_player_move/2, get_computer_move/2]).
 
 get_player_move(Board, View) ->
   AvailableSpaces = board:get_available_spaces(Board, []),
   Move = View:request_space(AvailableSpaces),
   board:place_marker(Board, Move, "X").
+
+get_computer_move(Board, Marker) ->
+  computer_logic:make_best_move(Board, Marker).
--- test/game_tests.erl
+++ test/game_tests.erl
@@ -1,10 +1,17 @@
 -module(game_tests).
 -include_lib("eunit/include/eunit.hrl").
--compile({nowarn_unused_function, [get_player_move_test/0]}).
+-compile({nowarn_unused_function, [get_player_move_test/0, get_computer_move_test/0]}).
 
 get_player_move_test() ->
   {"it will return an updated board with player move",
     ?assertEqual([1,"X",3,4,5,6,7,8,9], 
                   game:get_player_move(_Board = [1,2,3,4,5,6,7,8,9],
                                        _View = mock_io_handler))
+  }.
+
+get_computer_move_test() ->
+  {"it will return an updated board with computer move",
+    ?assertEqual(["O","X",3,4,5,6,7,8,9],
+                  game:get_computer_move(_Board = [1,"X",3,4,5,6,7,8,9],
+                                         _Marker = "O"))
   }.
