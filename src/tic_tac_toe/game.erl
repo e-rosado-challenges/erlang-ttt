8af1ea2f1e4c82351de0b03aac8971b1e5aa1441
ericrosado <erosado@8thlight.com>
2018-09-20 00:45:23 -0500

    Refactor play_game function.

--- src/tic_tac_toe/game.erl
+++ src/tic_tac_toe/game.erl
@@ -1,5 +1,6 @@
 -module(game).
--export([initialize_game/1, get_player_move/3, get_computer_move/2, play_game/4]).
+-export([initialize_game/1, get_player_move/3, get_computer_move/2,
+         play_game/4]).
 
 initialize_game(View) ->
   Marker = "X",
@@ -8,14 +9,18 @@ initialize_game(View) ->
   play_game(Marker, Board, View, GameOver).
 
 play_game(Marker, _Board, View, {Condition, GameOver}) when GameOver ->
+  LastActiveMarker = board:switch_marker(Marker),
   case Condition of
     tie_game -> View:print_tie();
-    _ -> View:print_winner(Marker)
+    _ -> View:print_winner(LastActiveMarker)
   end;
-play_game(Marker, Board, View, _GameOver) ->
+play_game(Marker, Board, View, _GameOver) when Marker =:= "X" ->
   PlayerUpdatedBoard = get_player_move(Marker, Board, View),
   ComputerMarker = board:switch_marker(Marker),
-  ComputerUpdatedBoard = get_computer_move(PlayerUpdatedBoard, ComputerMarker),
+  play_game(ComputerMarker, PlayerUpdatedBoard, View,
+            end_game_conditions:is_game_over(PlayerUpdatedBoard));
+play_game(Marker, Board, View, _GameOver) when Marker =:= "O" ->
+  ComputerUpdatedBoard = get_computer_move(Board, Marker),
   PlayerMarker = board:switch_marker(Marker),
   play_game(PlayerMarker, ComputerUpdatedBoard, View,
             end_game_conditions:is_game_over(ComputerUpdatedBoard)).
